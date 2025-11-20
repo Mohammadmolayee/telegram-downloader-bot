6bea1f07607fb5f901e73d53945556bc9551c3a0
Mohammadmolayee <143947402+Mohammadmolayee@users.noreply.github.com>
2025-11-20 20:33:06 +0330

    config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+# config.py
+
+BOT_NAME = "دانلودر حرفه‌ای"
+
+GUEST_LIMIT = 15
+USER_LIMIT = 25
+
+DATABASE_PATH = "database.db"
+DOWNLOAD_FOLDER = "downloads"
+
+SUPPORTED_VIDEO = ["youtube", "youtu", "tiktok", "instagram"]
+SUPPORTED_AUDIO = ["soundcloud", "spotify"]
